eee97a64aa55ea38be749ab6e54aea99f0605973
Bruno C <brunoramoscorreia15@gmail.com>
2025-07-22 20:27:46 -0300

    feat: update Index component with layout and styles

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,70 @@
-import { Text, View } from "react-native";
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Helow Word!!!</Text>
+    <View style={styles.container}>
+      <Image  style={styles.image} source={require('./foco.png')}/>
+      <View style={styles.action}>
+        <Text style={styles.timer}>
+          25:00
+        </Text>
+        <Pressable style={styles.button}>
+          <Text style={styles.textButton}>Começar</Text>
+        </Pressable>
+      </View> 
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>
+          Projeto fictício e sem fins comerciais.
+        </Text>
+        <Text style={styles.footerText}>
+          Desenvolvido por Alura.
+        </Text>
+      </View>
     </View>
-  );
+  ); 
 }
+  
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#021123",
+    gap: 40,
+  },
+  action:{
+    padding: 20,
+    backgroundColor: "#14448080",
+    width: "80%",
+    borderRadius: 32,
+    borderWidth: 2,
+    borderColor: "#144480",
+    gap: 32
+  },
+  timer:{
+    fontSize: 54,
+    color: "#fff",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  button: {
+    backgroundColor: "#B872FF",
+    padding: 8,
+    borderRadius: 32,
+    alignItems: "center",
+  },
+  textButton:{
+    textAlign: "center",
+    color: "#021123",
+    fontSize: 18
+  },
+  footer:{
+    width: "80%",
+  },
+  footerText:{
+    textAlign: "center",
+    color: "#98A0A8",
+    fontSize: 12.5
+  }
+});
+  
