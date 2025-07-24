df6bb92e156e2e27c184a826f3503392a6367ecd
Bruno C <brunoramoscorreia15@gmail.com>
2025-07-23 22:35:26 -0300

    feat: add pomodoro timer images and enhance Index component functionality

--- app/index.jsx
+++ app/index.jsx
@@ -1,17 +1,55 @@
-import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { Dimensions, Image, Pressable, StyleSheet, Text, View } from "react-native";
+const { width } = Dimensions.get('window');
+const pomodoro = [
+  {
+    id: 'pomodo',
+    time: 25,
+    img: require('./foco.png'),
+    text: 'Foco'
+  },
+  {
+    id: 'short',
+    time: 5,
+    img: require('./short.png'),
+    text: 'Pausa curta'
+  },
+  {
+    id: 'long',
+    time: 15,
+    img: require('./long.png'),
+    text: 'Pausa longa'
+  },
+]
 
 export default function Index() {
+
+  const [timerType, setTimerTipe] = useState(pomodoro[0])
+
   return (
     <View style={styles.container}>
-      <Image  style={styles.image} source={require('./foco.png')}/>
+      <Image style={styles.image} source={timerType.img} />
       <View style={styles.action}>
+        <View style={styles.context}>
+          { pomodoro.map(p => (
+            <Pressable 
+              key={p.id} 
+              style={ timerType.id === p.id ? styles.contextButtonActive : null}
+              onPress={() => setTimerTipe(p)}
+              >
+              <Text style={styles.contextButtonText}>
+                {p.text}
+              </Text>
+            </Pressable>
+          ))}
+        </View>
         <Text style={styles.timer}>
-          25:00
+          { new Date(timerType.time * 1000).toLocaleTimeString('pt-BR', {minute: '2-digit', second: '2-digit'}) }
         </Text>
         <Pressable style={styles.button}>
           <Text style={styles.textButton}>Começar</Text>
         </Pressable>
-      </View> 
+      </View>
       <View style={styles.footer}>
         <Text style={styles.footerText}>
           Projeto fictício e sem fins comerciais.
@@ -21,9 +59,9 @@ export default function Index() {
         </Text>
       </View>
     </View>
-  ); 
+  );
 }
-  
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -32,7 +70,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#021123",
     gap: 40,
   },
-  action:{
+  action: {
     padding: 20,
     backgroundColor: "#14448080",
     width: "80%",
@@ -41,7 +79,21 @@ const styles = StyleSheet.create({
     borderColor: "#144480",
     gap: 32
   },
-  timer:{
+  context: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  contextButtonActive: {
+    backgroundColor: "#144480",
+    borderRadius: 8,
+  },
+  contextButtonText: {
+    fontSize: 12.5,
+    color: '#fff',
+    padding: 8
+  },
+  timer: {
     fontSize: 54,
     color: "#fff",
     fontWeight: "bold",
@@ -53,18 +105,22 @@ const styles = StyleSheet.create({
     borderRadius: 32,
     alignItems: "center",
   },
-  textButton:{
+  textButton: {
     textAlign: "center",
     color: "#021123",
     fontSize: 18
   },
-  footer:{
+  footer: {
     width: "80%",
   },
-  footerText:{
+  footerText: {
     textAlign: "center",
     color: "#98A0A8",
     fontSize: 12.5
+  },
+  image: {
+    width: width * 0.7, // 60% da largura da tela
+    height: width * 0.7,
+    resizeMode: 'contain'
   }
 });
-  
